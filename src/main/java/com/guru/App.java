d8a9588d5a4f9a799f21fdae51bcbc76e94cff25
senguptaaritra <33267621+senguptaaritra@users.noreply.github.com>
2021-07-07 22:57:24 -0700

    Update App.java

--- src/main/java/com/guru/App.java
+++ src/main/java/com/guru/App.java
@@ -13,6 +13,10 @@ public class App
 	public static void bad() throws NoSuchAlgorithmException, NoSuchPaddingException {
 		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
 	}
+	
+	public static void badTwo() throws NoSuchAlgorithmException, NoSuchPaddingException {
+		Cipher cipher = Cipher.getInstance("blah");
+	}
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
 		System.out.println("Welcome to this little App!");
